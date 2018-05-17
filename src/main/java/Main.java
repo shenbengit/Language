89eaf568b962c6d7b73c58c5e7b1702a589a23af
Ben <714081644@qq.com>
2018-05-17 14:14:15 +0800

    重写解析方法，可以一起解析。

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,6 @@ public class Main {
         manager.setIdColumn(0);
         //单独解析，解析第几列的内容
         manager.setTargetColumn(2);
-        //为true时，setTargetColumn(),无效，如果列数大于5，需要一个一个解析，或者到analyse()中自行添加List<String>至mList中
-//        manager.setAnalysedAll(true);
         manager.setReaderConfig("E:\\strings.xls","E:\\strings.txt");
         manager.analyse();
     }
